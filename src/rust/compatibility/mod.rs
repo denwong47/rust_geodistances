9dcfa6c3cb7645a61c7605b880cb5111d69bd52c
Denny Wong <denwong47@hotmail.com>
2022-12-26 11:28:11 +0000

    Tidied up unit tests in Rust.

--- src/rust/compatibility/mod.rs
+++ src/rust/compatibility/mod.rs
@@ -3,7 +3,6 @@
 /// ==========================================
 pub mod conversions;
 pub mod enums;
-// pub mod func;
 
 // Import this if you want CalculationMethod to have Python Methods.
 pub mod python;
@@ -16,18 +15,3 @@ pub use enums::{
     CalculationInterfaceInternal,
     CalculationMethod,
 };
-
-// pub use func::{
-//     distance,
-//     distance_from_point,
-//     within_distance,
-//     within_distance_of_point,
-//     indices_within_distance,
-//     indices_within_distance_of_point,
-//     offset,
-//     offset_from_point,
-// };
-
-// pub mod stdin;
-// pub mod stdout;
-// pub mod pickle;
